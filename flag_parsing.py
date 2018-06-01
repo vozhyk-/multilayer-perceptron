8309281a69b84617fad1513e52e8447c8dd3d706
Vitaut Bajaryn <vitaut.bayaryn@gmail.com>
2018-06-01 19:18:32 +0200

    flag_parsing: Convert colors to numbers

--- flag_parsing.py
+++ flag_parsing.py
@@ -10,4 +10,18 @@ def parse(input: str):
 def parse_fields(row: List[str]):
     for i in it.chain(range(1, 17), range(18, 28)):
         row[i] = int(row[i])
+
+    colors = [
+        "black",
+        "blue",
+        "brown",
+        "gold",
+        "green",
+        "orange",
+        "red",
+        "white",
+    ]
+    for i in [17, 28, 29]:
+        row[i] = colors.index(row[i])
+
     return row
--- test_parse.py
+++ test_parse.py
@@ -12,4 +12,4 @@ Algeria,4,1,2388,20,8,2,2,0,3,1,1,0,0,1,0,0,green,0,0,0,0,1,1,0,0,0,0,green,whit
 
         parsed = flag_parsing.parse(input)
 
-        self.assertEqual(parsed[0], ["Afghanistan", 5, 1, 648, 16, 10, 2, 0, 3, 5, 1, 1, 0, 1, 1, 1, 0, "green", 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, "black", "green"])
+        self.assertEqual(parsed[0], ["Afghanistan", 5, 1, 648, 16, 10, 2, 0, 3, 5, 1, 1, 0, 1, 1, 1, 0, 4, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 4])
