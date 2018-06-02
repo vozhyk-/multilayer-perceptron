9c8b46fe504d31fc0dd9ac6e7c724a97cb357022
anshumanv93 <39656022+anshumanv93@users.noreply.github.com>
2018-06-02 19:52:56 +0200

    Test
    Test

---
+++ accuracy.py
@@ -0,0 +1,7 @@
+# Calculate accuracy percentage between two lists
+def accuracy_metric(actual, predicted):
+	correct = 0
+	for i in range(len(actual)):
+		if actual[i] == predicted[i]:
+			correct += 1
+	return correct / float(len(actual)) * 100.0
