69f94c488800b81c00be0ac040c6e84b2da00087
anshumanv93 <39656022+anshumanv93@users.noreply.github.com>
2018-06-02 20:01:35 +0200

    Test1

--- accuracy.py
+++ accuracy.py
@@ -1,3 +1,5 @@
+from math import sqrt
+
 # Calculate accuracy percentage between two lists
 def accuracy_metric(actual, predicted):
 	correct = 0
@@ -5,3 +7,43 @@ def accuracy_metric(actual, predicted):
 		if actual[i] == predicted[i]:
 			correct += 1
 	return correct / float(len(actual)) * 100.0
+
+# calculate a confusion matrix
+def confusion_matrix(actual, predicted):
+	unique = set(actual)
+	matrix = [list() for x in range(len(unique))]
+	for i in range(len(unique)):
+		matrix[i] = [0 for x in range(len(unique))]
+	lookup = dict()
+	for i, value in enumerate(unique):
+		lookup[value] = i
+	for i in range(len(actual)):
+		x = lookup[actual[i]]
+		y = lookup[predicted[i]]
+		matrix[x][y] += 1
+	return unique, matrix
+#Print confusion_matrix
+def print_confusion_matrix(unique, matrix):
+	print('(P)' + ' '.join(str(x) for x in unique))
+	print('(A)---')
+	for i, x in enumerate(unique):
+		print("%s| %s" % (x, ' '.join(str(x) for x in matrix[i])))
+
+def mae_metric(actual, predicted):
+	sum_error = 0.0
+	for i in range(len(actual)):
+		sum_error += abs(predicted[i] - actual[i])
+# Calculate mean absolute error
+def mae_metric(actual, predicted):
+	sum_error = 0.0
+	for i in range(len(actual)):
+		sum_error += abs(predicted[i] - actual[i])
+	return sum_error / float(len(actual))
+# Calculate root mean squared error
+def rmse_metric(actual, predicted):
+	sum_error = 0.0
+	for i in range(len(actual)):
+		prediction_error = predicted[i] - actual[i]
+		sum_error += (prediction_error ** 2)
+	mean_error = sum_error / float(len(actual))
+	return sqrt(mean_error)
