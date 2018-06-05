1ac04e840c734b008087ee3550e66299d78dfb79
Vitaut Bajaryn <vitaut.bayaryn@gmail.com>
2018-06-05 05:30:21 +0200

    mlpnolib: Make it import
    Fix indentation.
    Fix a mistake from copypasting.

--- mlpnolib.py
+++ mlpnolib.py
@@ -114,26 +114,26 @@ class Network:
         output.pop()# removing the bias neuron
         return output
 
-    def main():
-        topology = []
-        topology.append(2)
-        topology.append(3)
-        topology.append(2)
-        net = Network(topology)
-        Neuron.eta = 0.09
-        Neuron.alpha = 0.015
-        inputs = [[0, 0], [0, 1], [1, 0], [1, 1]]
-        outputs = [[0, 0], [1, 0], [1, 0], [0, 1]]
-        while True:
-            err = 0
+def main():
+    topology = []
+    topology.append(2)
+    topology.append(3)
+    topology.append(2)
+    net = Network(topology)
+    Neuron.eta = 0.09
+    Neuron.alpha = 0.015
+    inputs = [[0, 0], [0, 1], [1, 0], [1, 1]]
+    outputs = [[0, 0], [1, 0], [1, 0], [0, 1]]
+    while True:
+        err = 0
         for i in range(len(inputs)):
             net.setInput(inputs[i])
             net.feedForword()
             net.backPropagate(outputs[i])
             err = err + net.getError(outputs[i])
-            print ("error: ", err)
-            if (err &lt, 0.01):
-                break
+        print ("error: ", err)
+        if (err < 0.01):
+            break
 
     while True:
         a = input("type 1st input :")
